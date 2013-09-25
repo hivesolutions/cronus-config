da2d7402e0d6deb2e6270c192660cb2f9bc6572f
joamag <joamag@gmail.com>
2013-09-25 15:34:46 +0100

    intial configuration support for openid

---
+++ hive/pt.hive.colony.plugins.resources.manager/configuration/hive_openid/run_mode/hive/configuration.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Solutions Website
+# Copyright (C) 2010-2012 Hive Solutions Lda.
+#
+# This file is part of Hive Solutions Website.
+#
+# Hive Solutions Website is confidential and property of Hive Solutions Lda.,
+# its usage is constrained by the terms of the Hive Solutions
+# Confidential Usage License.
+#
+# Hive Solutions Website should not be distributed under any circumstances,
+# violation of this may imply legal action.
+#
+# If you have any questions regarding the terms of this license please
+# refer to <http://www.hive.pt/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2010-2012 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Hive Solutions Confidential Usage License (HSCUL)"
+""" The license for the module """
+
+configuration = {
+    "authentication_properties" : {
+        "authentication_handler" : "python",
+        "authentication_realm" : "system",
+        "arguments" : {
+            "file_path" : "%configuration:pt.hive.colony.plugins.authentication.python%/authentication.py"
+        }
+    }
+}
